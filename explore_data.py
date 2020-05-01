2f24815c4ea095fd3a62f3dd01bfc4a41e1f3abf
nafarmer <nafarmer@cisco.com>
2020-05-01 15:44:02 -0400

    import matplotlib

--- explore_data.py
+++ explore_data.py
@@ -3,6 +3,7 @@
 # Step 4: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Read in the csv file
@@ -10,3 +11,5 @@ if __name__ == '__main__':
     data = pd.read_csv(data_in_file)
 
     print(data.head())
+
+    
