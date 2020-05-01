90a3435c9c1d885755efc3b70c84e67ea47eeef8
Nathan Farmer <jnathanfarmer@gmail.com>
2020-05-01 15:27:50 -0400

    Documentation

--- integrate_data.py
+++ integrate_data.py
@@ -1,6 +1,6 @@
 # J. Nathan Farmer, Marko Milosavlijevic, Bright Phakamad, Matt Soria
 #
-# Step 4: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
+# Step 4: Run the file to integrate the datasets into a flat form for visualization
 
 import pandas
 
