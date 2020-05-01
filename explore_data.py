418675cc3657adbe6dfb12a4e3c640e766bcf22a
nafarmer <nafarmer@cisco.com>
2020-05-01 18:10:12 -0400

    What-if listwise deletion?

--- explore_data.py
+++ explore_data.py
@@ -14,17 +14,20 @@ if __name__ == '__main__':
     data = pd.read_csv(data_in_file)
 
     # What size are the different datasets?
-    print('Original Dataset Shape:', data.shape)
-    print('Extended Dataset Shape:', ext_data.shape)
+    print('Original dataset shape:', data.shape)
+    print('Extended dataset shape:', ext_data.shape)
 
     # Extended dataset has the same observations, just with more attributes associated
     # We will use the extended dataset and systematically remove fields we don't need
 
     # Missing values in ext_data
-    print('\nMissing Values Per Column:')
+    print('\nMissing values per column:')
     nulls = ext_data.isnull().sum().to_frame()
     for index, row in nulls.iterrows():
         print('{:30}{}'.format(index, row[0]))
 
     # All but 30 of the columns are missing MOST of the data
-    
+    # How many observations have data for EVERY column?
+    print('\nNumber of observations without missing data:', len(ext_data.dropna().index))
+
+    # We will need to decide 
