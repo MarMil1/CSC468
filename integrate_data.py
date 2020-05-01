609e11d8e74729bbb46c27e8fb60ad2c2a786f70
nafarmer <nafarmer@cisco.com>
2020-05-01 14:56:12 -0400

    Create integrate_data.py

---
+++ integrate_data.py
@@ -0,0 +1,6 @@
+# J. Nathan Farmer, Marko Milosavlijevic, Bright Phakamad, Matt Soria
+#
+# Step 4: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
+
+import pandas
+
