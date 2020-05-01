95c1d74da44928d59fd930c3115d4d5e287d256e
nafarmer <nafarmer@cisco.com>
2020-05-01 14:56:03 -0400

    Documentation of step number

--- download_data.py
+++ download_data.py
@@ -1,6 +1,6 @@
 # J. Nathan Farmer, Marko Milosavlijevic, Bright Phakamad, Matt Soria
 #
-# Step 1: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
+# Step 3: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
 
 import kaggle
 import os
