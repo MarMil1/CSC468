20cc8244a52f62f2f584d50e5ab1caa0214e4f17
Nathan Farmer <jnathanfarmer@gmail.com>
2020-05-03 11:30:29 -0400

    Moderate and weak correlation analysis

--- explore_data.py
+++ explore_data.py
@@ -3,6 +3,7 @@
 # Step 4: Run this file to download the dataset to your local machine in the correct folder that will be ignored by Git
 
 import pandas as pd
+import numpy as np
 #import matplotlib.pyplot as plt
 #import seaborn as sb
 
@@ -21,6 +22,12 @@ if __name__ == '__main__':
     # Extended dataset has the same observations, just with more attributes associated
     # We will use the extended dataset and systematically remove fields we don't need
 
+    ############################################################################################
+    #
+    # CHECK FOR MISSING VALUES
+    #
+    ############################################################################################
+
     # Missing values in ext_data
     print('\nMissing values per column:')
     nulls = ext_data.isnull().sum().to_frame()
@@ -31,6 +38,49 @@ if __name__ == '__main__':
     # How many observations have data for EVERY column?
     print('\nNumber of observations without missing data:', len(ext_data.dropna().index))
 
+    ############################################################################################
+    #
+    # CORRELATION ANALYSIS
+    #
+    ############################################################################################
+
+    print('\nTotal number of features:', len(ext_data.columns))
+
     # Before we can decide which columns or observations to drop, lets see how they correlate
-    ext_data_corr = ext_data.corr()
-    ext_data_corr.to_csv('data/ext_data_corr.csv')
+    ext_data_corr = ext_data.corr(method='pearson', min_periods=20)
+    # Output complete correlation matrix to csv file
+    ext_data_corr.to_csv('data/ext_data_corr_all.csv')
+    
+    # We will drop features with poor correlations accross the board
+    # Find the absolute value of each correlation
+    ext_data_corr_abs = ext_data_corr.abs()
+    # Select upper triangle of correlation matrix
+    upper = ext_data_corr_abs.where(np.triu(np.ones(ext_data_corr_abs.shape), k=1).astype(np.bool))
+    # Find features with all correlations between -0.3 and 0.3
+    to_drop = [column for column in upper.columns if not any(upper[column] > 0.3)]
+    # Drop uncorrelated features and run correlation analysis again
+    ext_data_weak = ext_data.drop(to_drop, axis=1)
+    ext_data_corr_weak = ext_data_weak.corr(method='pearson', min_periods=20)
+    print('Number of features with at least 1 correlation > 0.3:', len(ext_data_weak.columns))
+    # Output weak correlation matrix to csv file
+    ext_data_corr_weak.to_csv('data/ext_data_corr_weak.csv')
+
+    # We will drop features with less than moderate correlations accross the board
+    # Find features with all correlations between -0.6 and 0.6
+    to_drop = [column for column in upper.columns if not any(upper[column] > 0.6)]
+    # Drop uncorrelated features and run correlation analysis again
+    ext_data_moderate = ext_data.drop(to_drop, axis=1)
+    ext_data_corr_moderate = ext_data_moderate.corr(method='pearson', min_periods=20)
+    print('Number of features with at least 1 correlation > 0.6:', len(ext_data_moderate.columns))
+    # Output weak correlation matrix to csv file
+    ext_data_corr_moderate.to_csv('data/ext_data_corr_moderate.csv')
+
+    # We will drop features with less than moderate correlations accross the board
+    # Find features with all correlations between -0.9 and 0.9
+    to_drop = [column for column in upper.columns if not any(upper[column] > 0.9)]
+    # Drop uncorrelated features and run correlation analysis again
+    ext_data_strong = ext_data.drop(to_drop, axis=1)
+    ext_data_corr_strong = ext_data_strong.corr(method='pearson', min_periods=20)
+    print('Number of features with at least 1 correlation > 0.9:', len(ext_data_strong.columns))
+    # Output weak correlation matrix to csv file
+    ext_data_corr_strong.to_csv('data/ext_data_corr_strong.csv')
